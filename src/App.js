629cf928014e91cdd17ef2f703710dc5849326cb
Sanjay Narayana <sanjay.narayana@hotmail.com>
2019-09-21 16:10:40 +1000

    moved favicon credit inside app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <Birds/>
+      <div style="color: darkgrey">
+        Favicon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
+        href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </div>
     </div>
   );
 }
